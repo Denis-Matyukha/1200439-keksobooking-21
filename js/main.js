a6870ec8fb352f5fa2114056fc46c9089b012605
Denis-Matyukha <denmatuhawork@gmail.com>
2020-11-10 01:55:10 +0300

    middle workable state before DRYing ang comments removing

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const housingType = mainFormElement.querySelector(`#type`);
 const timeIn = mainFormElement.querySelector(`#timein`);
 const timeOut = mainFormElement.querySelector(`#timeout`);
 
-const publishButton = mainFormElement.querySelector(`.ad-form__submit`);
+// const publishButton = mainFormElement.querySelector(`.ad-form__submit`);
 
 const filterHousingType = mapFilterForm.querySelector(`#housing-type`);
 
@@ -30,57 +30,58 @@ const checkForm = function () {
 
 //
 //
-const sendForm = function () {
-  // let URL = `https://21.javascript.pages.academy/keksobooking`;
+// const sendForm = function () {
+// let URL = `https://21.javascript.pages.academy/keksobooking`;
 
 
-};
+// };
 
 
 // 'use strict';
 
 // (function () {
-  let URL = `https://21.javascript.pages.academy/keksobooking`;
 
-  window.utilityUpload = function (data, onSuccess) {
+// let URL = `https://21.javascript.pages.academy/keksobooking`;
+let URL = `https://21.javascript.pages.academy/keksobookingXXX`;
 
-    console.log(`вот сейчас сработала функция window.utilityUpload(data, onSuccess)`);
-    console.log(`в которой data это ↓ `);
-    console.log(data);
+window.utilityUpload = function (data, onSuccess) {
 
-    let xhr = new XMLHttpRequest();
+  console.log(`вот сейчас сработала функция window.utilityUpload(data, onSuccess)`);
+  console.log(`в которой data это ↓ `);
+  console.log(data);
 
-    console.log(`вот сформировался не такой уж и пустой XMLHttpRequest - xhr ↓`);
+  let xhr = new XMLHttpRequest();
+
+  console.log(`вот сформировался не такой уж и пустой XMLHttpRequest - xhr ↓`);
+  console.log(xhr);
+
+  xhr.responseType = `json`;
+
+  console.log(`после команды xhr.responseType = json; объект примет видxhr ↓`);
+  console.log(xhr);
+
+  xhr.addEventListener(`load`, function (){
+    console.log(`а сейчас сработало событие xhr load и отрабтает функция onSuccess задействовав xhr.response`);
+    console.log(`xhr.statusText -> ${xhr.statusText}`);
+    console.log(`xhr.status -> ${xhr.status}`);
+    console.log(`==================`);
+    console.log(`↓↓↓ xhr ↓↓↓`);
     console.log(xhr);
+    onSuccess(xhr);
+  });
 
-    xhr.responseType = `json`;
+  xhr.open(`POST`, URL);
+  xhr.send(data);
+  console.log(`в этот момент произошла отправка data через xhr по адресу URL ↓`);
+  console.log(URL);
 
-    console.log(`после команды xhr.responseType = json; объект примет видxhr ↓`);
-    console.log(xhr);
-
-    xhr.addEventListener(`load`, function (){
-      console.log(`а сейчас сработало событие xhr load и отрабтает функция onSuccess задействовав xhr.response`);
-      console.log(`xhr.statusText -> ${xhr.statusText}`);
-      console.log(`xhr.status -> ${xhr.status}`);
-      onSuccess(xhr.response);
-    });
-
-    xhr.open(`POST`, URL);
-    xhr.send(data);
-    console.log(`в этот момент произошла отправка data через xhr по адресу URL ↓`);
-    console.log(URL);
-
-  };
+};
 // })();
 
-
-//
-//
-
-const checkAndSendForm = function () {
-  checkForm();
-  sendForm();
-};
+// const checkAndSendForm = function () {
+//   checkForm();
+//   sendForm();
+// };
 
 const successHandler = function (advertisementArray) {
   window.fullAdvertisementArray = advertisementArray;
@@ -97,12 +98,9 @@ const errorHandler = function (errorMessage) {
 
 const activatePage = function () {
   if (!activateFlag) {
-
     window.utilityLoad.getXHRequest(successHandler, errorHandler);
-
     window.utilityForm.toggleDisableAttr(mapFilters);
     window.utilityForm.toggleDisableAttr(formInputs);
-
     window.utilityForm.onChangeRoomsHolder(roomsQuantity, guestsQuantity)();
     activateFlag = true;
   }
@@ -111,35 +109,87 @@ const activatePage = function () {
   window.utilityForm.setTargetCords(adressArea, mainPin, window.utilityData.PIN_BOTTOM_HEIGHT);
 };
 
+const deActivatePage = function () {
+  window.utilityForm.toggleDisableAttr(mapFilters);
+  window.utilityForm.toggleDisableAttr(formInputs);
+  activateFlag = false;
+  mapBlock.classList.add(`map--faded`);
+  mainFormElement.classList.add(`ad-form--disabled`);
+  window.utilityCard.removeExistedAdvCard();
+  window.utilityCard.removeExistedPins();
+};
+
 
 window.utilityForm.toggleDisableAttr(mapFilters);
 window.utilityForm.toggleDisableAttr(formInputs);
 
 window.utilityForm.setTargetCords(adressArea, mainPin, window.utilityData.PIN_BOTTOM_HEIGHT);
 
-// publishButton.addEventListener(`click`, checkForm);
-// publishButton.addEventListener(`click`, checkAndSendForm);
-// mainFormElement.addEventListener(`submit`, checkAndSendForm);
+//
+//
 mainFormElement.addEventListener(`submit`, function (evt) {
   window.utilityUpload(new FormData(mainFormElement), function (response) {
+    // добавить события для xhr на error, timeout и прочее
+    console.log(`работаем внутри callbacka с response.status`);
+    console.log(response.status);
+    if(response.status === 200) {
+      console.log(`response.status === 200`);
+      console.log(`response.statusText -> ${response.statusText}`);
+      mainFormElement.reset();
+      deActivatePage();
+      let successMessageElement = document.querySelector(`#success`).content.querySelector(`.success`).cloneNode(true);
+      document.body.insertAdjacentElement(`afterbegin`, successMessageElement);
+      document.body.addEventListener(`keydown`, function(evt) {
+        if (evt.key === window.utilityData.EVENT_CODE.KEYBOARD_ESCAPE) {
+          successMessageElement.remove();
+        }
+      }, {once: true});
+      document.body.addEventListener(`click`, function() {
+        successMessageElement.remove();
+      }, {once: true});
+    } else {
+      console.warn(`response.status -> ${response.status}`);
+      console.warn(`response.statusText -> ${response.statusText}`);
 
-    console.log(response);
-    console.log(document.querySelector(`#success`).content);
-    // сбросить форму
-    // перевести страницу в неактивное состояние
-    // вывести сообщение success
-    // document.body.insertAdjacentHTML(`afterbegin`, document.querySelector(`#success`).content);
-    let messageElement = document.querySelector(`#success`).content.querySelector(`.success`);
+      let errorMessageElement = document.querySelector(`#error`).content.querySelector(`.error`).cloneNode(true);
+      let errorButton = errorMessageElement.querySelector(`.error__button`);
+      document.querySelector(`main`).insertAdjacentElement(`beforeend`, errorMessageElement);
+      errorButton.addEventListener(`click`, function() {
+        errorMessageElement.remove();
+      });
+      document.body.addEventListener(`keydown`, function(evt) {
+        if (evt.key === window.utilityData.EVENT_CODE.KEYBOARD_ESCAPE) {
+          errorMessageElement.remove();
+        }
+      }, {once: true});
+      document.body.addEventListener(`click`, function() {
+        errorMessageElement.remove();
+      }, {once: true});
+    }
 
-    document.body.insertAdjacentElement(`afterbegin`, messageElement);
+    // mainFormElement.reset();
+    // deActivatePage();
+    // let successMessageElement = document.querySelector(`#success`).content.querySelector(`.success`).cloneNode(true);
+    // document.body.insertAdjacentElement(`afterbegin`, successMessageElement);
 
-    // setTimeout(function(){
-    //   messageElement.remove();
-    // },1500);
+
+    // document.body.addEventListener(`keydown`, function(evt) {
+    //   if (evt.key === window.utilityData.EVENT_CODE.KEYBOARD_ESCAPE) {
+    //     successMessageElement.remove();
+    //   }
+    // }, {once: true});
+
+
+    // document.body.addEventListener(`click`, function() {
+    //   successMessageElement.remove();
+    // }, {once: true});
+
 
   });
   evt.preventDefault();
 });
+//
+//
 
 housingType.addEventListener(`change`, window.utilityForm.onChangeTypeHolder(priceElem));
 
